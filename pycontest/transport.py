c1090b610ebbaea706de58a269952874f8784e4a
sruud <13337210+sruud@users.noreply.github.com>
2025-06-24 21:44:43 -0600

    test: add transport tests and improve function type handling

--- pycontest/transport.py
+++ pycontest/transport.py
@@ -15,7 +15,20 @@ def transport(loc, vel, dt):
     if isinstance(loc, (int, float)) and isinstance(vel, (int, float)):
         loc = loc + vel * dt
 
+    # Added block for -> test_transport_4
+    # if loc or vel is list, we want to convert to numpy arrays
+    if type(loc) is list:
+        loc = np.array(loc)
+    if type(vel) is list:
+        vel = np.array(vel)
+
     if isinstance(loc, np.ndarray) and isinstance(vel, np.ndarray):
+        # vel = vel.astype(np.float32, copy=False)
+        
+        # fixing for xfail "doesn't work for arrays with dtype=int"
+        vel = vel.astype(dtype=np.float32, copy=False)
+        loc = loc.astype(dtype=np.float32, copy=False)
+
         vel = vel.astype(np.float32, copy=False)
         loc[:] = loc[:] + vel[:] *dt
     return loc
--- tests/test_transport.py
+++ tests/test_transport.py
@@ -4,31 +4,49 @@ import pytest
 from pycontest.transport import transport
 
 
-# # first process is transport, and we can easily write a simple test
-# def test_transport_1():
-#     loc = 3.
-#     vel = 1.
-#     dt = .5
-#     assert transport(loc, vel, dt) == 3.5
-#
-# # but we also want to check if it works for arrays with 2d
-# def test_transport_2():
-#     loc = np.array([[1, 2], [11, 12]])
-#     vel = np. array([[1, 1], [-1, -1]])
-#     dt = 1
-#     assert (transport(loc, vel, dt) == np.array([[2, 3], [10, 11]])).all()
-#
-#
-# # let's try one more, with dt that is float
-# def test_transport_3():
-#     loc = np.array([[1, 2], [11, 12]])
-#     vel = np. array([[1, 1], [-1, -1]])
-#     dt = .5
-#     assert (transport(loc, vel, dt) == np.array([[1.5, 2.5], [10.5, 11.5]])).all()
-#
+# first process is transport, and we can easily write a simple test
+def test_transport_1():
+    loc = 3.
+    vel = 1.
+    dt = .5
+    
+    # assert transport(loc, vel, dt) == 3.5
+
+    # solution
+    lof_final = 3 + 0.5 * 1
+    assert transport(loc=loc, vel=vel, dt=dt) == lof_final
+
+# but we also want to check if it works for arrays with 2d
+def test_transport_2():
+    loc = np.array([[1, 2], [11, 12]])
+    vel = np.array([[1, 1], [-1, -1]])
+    dt = 1
+    assert (transport(loc, vel, dt) == np.array([[2, 3], [10, 11]])).all()
+
+# let's try one more, with dt that is float
+# Example on how to use a decorator with a currently failing test
+# @pytest.mark.xfail(reason="doesn't work for arrays with dtype=int")
+def test_transport_3():
+    loc = np.array([[1, 2], [11, 12]])
+    vel = np.array([[1, 1], [-1, -1]])
+    dt = .5
+    assert (transport(loc, vel, dt) == np.array([[1.5, 2.5], [10.5, 11.5]])).all()
+
 # # and now let's try with lists
-# def test_transport_4():
-#     loc = [[1, 2], [11, 12]]
-#     vel = [[1, 1], [-1, -1]]
-#     dt = .5
-#     assert (transport(loc, vel, dt) == np.array([[1.5, 2.5], [10.5, 11.5]])).all()
+# fix not shown in workshop: TODO
+def test_transport_4():
+    loc = [[1, 2], [11, 12]]
+    vel = [[1, 1], [-1, -1]]
+    dt = .5
+    assert (transport(loc, vel, dt) == np.array([[1.5, 2.5], [10.5, 11.5]])).all()
+
+
+# NOTES
+# pytest decorators - mark
+# marking tests for a known fail / fix later
+# @pytest.mark.xfail(reason="doesn't work for arrays with dtype=int")
+
+# https://docs.pytest.org/en/stable/reference/reference.html#marks
+
+# read about skip/xfail
+# https://docs.pytest.org/en/stable/how-to/skipping.html
